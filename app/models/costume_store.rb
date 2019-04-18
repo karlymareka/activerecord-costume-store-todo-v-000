3c215aa6b1a9c73bd33e11785197572208c54372
Karly Mareka <karly.mareka@gmail.com>
2019-04-18 21:33:47 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -11,7 +11,7 @@ class CreateCostumeStore < ActiveRecord::Migration[4.2]
       t.integer :costume_inventory
       t.integer :employee_count
       t.boolean :still_in_business?
-      t. datetime :opening_time
+      t.datetime :opening_time
       t.datetime :closing_time 
   end 
 end 
