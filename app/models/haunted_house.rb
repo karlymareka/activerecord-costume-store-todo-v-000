8606fa5b1534fefae7c7bb7a811fac890fee1dc5
Karly Mareka <karly.mareka@gmail.com>
2019-04-18 21:43:29 +0000

    Automatically backed up by Learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -3,7 +3,7 @@ class HauntedHouse < ActiveRecord::Base
   
 end 
 
-class CreateHauntedHouses < ActiveRecord::Migrations[4.2] 
+class CreateHauntedHouses < ActiveRecord::Migration[4.2] 
   def change 
     create_table :haunted_houses do |t|
       t.string :name 
