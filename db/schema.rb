1be61512cb98c921f3c760cb35ea418d1c926b87
Karly Mareka <karly.mareka@gmail.com>
2019-04-18 21:47:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.integer "costume_inventory"
     t.integer "num_of_employees"
-    t.string "still_in_business"
+    t.boolean "still_in_business"
     t.datetime "opening_time"
     t.datetime "closing_time"
   end
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.string "theme"
     t.integer "price"
-    t.string "family_friendly"
+    t.boolean "family_friendly"
     t.datetime "opening_date"
     t.datetime "closing_date"
   end
