d838fe67d510196aa8af4022434ef91c79194a89
Karly Mareka <karly.mareka@gmail.com>
2019-04-18 21:45:00 +0000

    Automatically backed up by Learn

--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -6,7 +6,13 @@ end
 class CreateHauntedHouses < ActiveRecord::Migration[4.2] 
   def change 
     create_table :haunted_houses do |t|
-      t.string :name 
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
+      t.string :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date 
     end 
   end 
 end 
