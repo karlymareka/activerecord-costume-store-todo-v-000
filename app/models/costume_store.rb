b1226b2a19cd45aefe7b3cd87c9a573da3114bf2
Karly Mareka <karly.mareka@gmail.com>
2019-04-18 21:35:17 +0000

    Automatically backed up by Learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -5,7 +5,7 @@ end
 
 class CreateCostumeStore < ActiveRecord::Migration[4.2]
   def change 
-    create_table :costume_store do |t|
+    create_table :costume_stores do |t|
       t.string :name 
       t.string :location
       t.integer :costume_inventory
